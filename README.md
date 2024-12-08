56123ef11b586ef73a8732cbd02779fff450a54c
yacine-zitouni <yacine-zitouni@etu.usthb.dz>
2024-12-08 01:40:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yacine_coffeeshop
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/yacine_coffeeshop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yacine_coffeeshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
